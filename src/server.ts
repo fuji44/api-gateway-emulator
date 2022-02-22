59b350a8a205418b0029f6f8115d46136a17f147
fuji44 <fuji.44mt@gmail.com>
2022-02-22 03:36:34 +0000

    feat: implemented dummy jwt mode

--- src/server.ts
+++ src/server.ts
@@ -1,70 +1,153 @@
-import express, { Request } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import jwt from "express-jwt";
 import JwksRsa from "jwks-rsa";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import base64url from "base64url";
-
-const VERBOSE = process.env.VERBOSE ? Boolean(process.env.VERBOSE) : false;
-
-const PORT = process.env.PORT || 3000;
-const TARGET_URL = process.env.TARGET_URL;
-const BASE_PATH = process.env.BASE_PATH || "/";
-
-const OAUTH_JWKS_URI = process.env.OAUTH_JWKS_URI;
-const OAUTH_ISSUER = process.env.OAUTH_ISSUER;
-const OAUTH_AUDIENCE = process.env.OAUTH_AUDIENCE;
-const OAUTH_TOKEN_SIGN_ALGORITHMS = process.env.OAUTH_TOKEN_SIGN_ALGORITHMS
-  ? process.env.OAUTH_TOKEN_SIGN_ALGORITHMS.split(",")
-  : ["RS256"];
-
-if (!TARGET_URL || !OAUTH_JWKS_URI || !OAUTH_ISSUER || !OAUTH_AUDIENCE) {
-  console.error(
-    "\x1b[31m",
-    "Error: Required environment variables [TARGET_URL, OAUTH_JWKS_URI, OAUTH_ISSUER, OAUTH_AUDIENCE] are not defined."
-  );
-  process.exit(1);
-}
-
-const app = express();
-app.use(
-  jwt({
-    secret: JwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: OAUTH_JWKS_URI,
-    }),
-    issuer: OAUTH_ISSUER,
-    audience: OAUTH_AUDIENCE,
-    algorithms: OAUTH_TOKEN_SIGN_ALGORITHMS,
-  })
-);
+import { readFileSync } from "fs";
 
 type AuthenticatedRequest = Request & { user: object };
 
-app.use(
-  BASE_PATH,
-  createProxyMiddleware({
-    target: TARGET_URL,
-    changeOrigin: true,
-    onProxyReq: (proxyReq, req, res, options) => {
-      if (VERBOSE) {
-        console.info(`before headers:`, proxyReq.getHeaders());
+function createAuthMiddleware(authType: string): express.RequestHandler {
+  switch (authType) {
+    case "dummy_jwt": {
+      const jwtJsonPath = process.env.DUMMY_JWT_JSON;
+      if (!jwtJsonPath) {
+        console.error(
+          "\x1b[31m",
+          "Error: If AUTH_TYPE=dummy_jwt, the environment variable DUMMY_JWT_JSON needs to be set to the path of the JWT file in JSON format."
+        );
+        process.exit(1);
+      }
+      let jwt: object;
+      try {
+        jwt = JSON.parse(readFileSync(jwtJsonPath, "utf-8"));
+      } catch (error: any) {
+        if (error.code && error.code == "ENOENT") {
+          console.error(
+            "\x1b[31m",
+            "Error: The file set to DUMMY_JWT_JSON cannot be found.",
+            jwtJsonPath
+          );
+          process.exit(1);
+        }
+        if (error instanceof SyntaxError) {
+          console.error(
+            "\x1b[31m",
+            "Error: Failed to parse JSON.",
+            error.message,
+            jwtJsonPath
+          );
+          process.exit(1);
+        }
+        throw error;
+      }
+      return (req, res, next) => {
+        (req as AuthenticatedRequest).user = jwt;
+        next();
+      };
+    }
+
+    case "jwks": {
+      const jwksUri = process.env.OAUTH_JWKS_URI;
+      const issuer = process.env.OAUTH_ISSUER;
+      const audience = process.env.OAUTH_AUDIENCE;
+      const algorithms = process.env.OAUTH_TOKEN_SIGN_ALGORITHMS
+        ? process.env.OAUTH_TOKEN_SIGN_ALGORITHMS.split(",")
+        : ["RS256"];
+
+      if (!jwksUri || !issuer || !audience) {
+        console.error(
+          "\x1b[31m",
+          "Error: If AUTH_TYPE=jwks, the environment variables [OAUTH_JWKS_URI, OAUTH_ISSUER, OAUTH_AUDIENCE] must be set."
+        );
+        process.exit(1);
       }
 
-      proxyReq.setHeader(
-        "x-apigateway-api-userinfo",
-        base64url.encode(JSON.stringify((req as AuthenticatedRequest).user))
+      return jwt({
+        secret: JwksRsa.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: jwksUri,
+        }),
+        issuer: issuer,
+        audience: audience,
+        algorithms: algorithms,
+      });
+    }
+
+    default:
+      console.error(
+        "\x1b[31m",
+        "Error: AUTH_TYPE must be one of [jwks, dummy_jwt]."
       );
-      const authorization = req.header("authorization");
-      if (authorization) {
-        proxyReq.setHeader("x-forwarded-authorization", authorization);
-      }
+      process.exit(1);
+  }
+}
 
-      if (VERBOSE) {
-        console.info(`after headers:`, proxyReq.getHeaders());
-      }
-    },
-  })
-);
-app.listen(PORT);
+function errorHandler(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.error("\x1b[31m", err.stack);
+  res.status(500);
+  if (err.message) {
+    res.send({ error: err.message });
+    return;
+  }
+  res.send({ error: err });
+}
+
+function run() {
+  const verbose = process.env.VERBOSE ? Boolean(process.env.VERBOSE) : false;
+  const targetUrl = process.env.TARGET_URL;
+  if (!targetUrl) {
+    console.error(
+      "\x1b[31m",
+      "Error: Required environment variables TARGET_URL are not defined."
+    );
+    process.exit(1);
+  }
+
+  const app = express();
+  const authType = process.env.AUTH_TYPE
+    ? process.env.AUTH_TYPE.toLowerCase()
+    : "jwks";
+  app.use(createAuthMiddleware(authType));
+  app.use(
+    process.env.BASE_PATH || "/",
+    createProxyMiddleware({
+      target: targetUrl,
+      changeOrigin: true,
+      onProxyReq: (proxyReq, req, res, options) => {
+        if (verbose) {
+          console.info(`before headers:`, proxyReq.getHeaders());
+        }
+
+        proxyReq.setHeader(
+          "x-apigateway-api-userinfo",
+          base64url.encode(JSON.stringify((req as AuthenticatedRequest).user))
+        );
+        const authorization = req.header("authorization");
+        if (authorization) {
+          proxyReq.setHeader("x-forwarded-authorization", authorization);
+        }
+
+        if (verbose) {
+          console.info(`after headers:`, proxyReq.getHeaders());
+        }
+      },
+    })
+  );
+  app.use(errorHandler);
+
+  const port = process.env.PORT || 3000;
+  app.listen(port);
+
+  console.log("Auth type:", authType);
+  console.log("Running Gateway:", `http://localhost:${port}`);
+}
+
+run();
